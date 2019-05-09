209af5e366f020d099a7da9a8bd60b9576b07e04
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-05-10 00:18:12 +0200

    publish new site version

--- docs/src.f69400ca.js
+++ docs/src.f69400ca.js
@@ -59379,7 +59379,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46579" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39743" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
