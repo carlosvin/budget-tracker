b1ef527c896ddbb2f99eb0618e79386a24bd3f03
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-05-17 23:53:42 +0200

    Change it to functional component

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -5,20 +5,18 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { TextButton } from "./buttons";
 
-export class Header extends React.PureComponent<{title: string}> {
-    render () {
-        return <AppBar position='sticky'>
-            <Toolbar>
-                <Grid container justify='space-between'>
-                    <Grid item>
-                        <Typography color='inherit' variant='h6'>{this.props.title}</Typography>
-                    </Grid>
-                    <Grid item>
-                        <TextButton color='inherit' href='/budgets' text='Budgets'/>
-                        <TextButton color='inherit' href='/categories' text='Categories'/>
-                    </Grid>
+export const Header = (props: {title: string}) => (
+    <AppBar position='sticky'>
+        <Toolbar>
+            <Grid container justify='space-between'>
+                <Grid item>
+                    <Typography color='inherit' variant='h6'>{props.title}</Typography>
                 </Grid>
-            </Toolbar>
-        </AppBar>;
-    }
-}
+                <Grid item>
+                    <TextButton color='inherit' href='/budgets' text='Budgets'/>
+                    <TextButton color='inherit' href='/categories' text='Categories'/>
+                </Grid>
+            </Grid>
+        </Toolbar>
+    </AppBar>
+);
