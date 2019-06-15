538cf8003eebab208d5377155e3196c8503f8e71
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-06-15 19:52:16 +0200

    Remove tabs, use menu instead

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,36 +1,23 @@
 import * as React from "react";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Tab from "@material-ui/core/Tab";
-import Tabs from "@material-ui/core/Tabs";
-import { MyLink } from "./MyLink";
+import { AppMenu } from "./Menu";
+import './Header.css';
+
+const MenuItems = [
+    {name: 'Budgets', href: '/budgets'},
+    {name: 'Categories', href: '/categories'},
+];
 
 export const Header = (props: {title: string, actions: React.ReactNode}) => {
-    const [value, setValue] = React.useState(0);
 
-    const handleChange = (event: React.ChangeEvent<{}>, value: number) => {
-        setValue(value);
-    }
-
-    return (<AppBar position='sticky'>
+    return (
+    <AppBar position='sticky'>
         <Toolbar>
-            <Grid container justify='space-between'>
-                <Grid item>
-                    <Typography color='inherit' variant='h6'>{props.title}</Typography>
-                </Grid>
-                <Grid item>
-                    {props.actions}
-                </Grid>
-            </Grid>
+            <AppMenu {...MenuItems}/>
+            <Typography color='inherit' variant='h6' className='headerAppTitle'>{props.title}</Typography>
+            {props.actions}
         </Toolbar>
-        <Tabs 
-            value={value} 
-            onChange={handleChange}
-            variant='fullWidth'>
-            <Tab component={MyLink} href='/budgets' label='Budgets' />
-            <Tab component={MyLink} href='/categories' label='Categories' />
-        </Tabs>
     </AppBar>);
 }
---
+++ src/views/Menu.tsx
@@ -0,0 +1,42 @@
+
+import * as React from "react";
+
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { MyLink } from "./MyLink";
+
+export const AppMenu: React.FC<{href: string, name: string}[]> = (props) => {
+
+    const [anchorEl, setAnchorEl] = React.useState<Element|null>(null);
+
+    const handleClick = (event: React.SyntheticEvent) => (setAnchorEl(event.currentTarget));
+  
+    const handleClose = () => (setAnchorEl(null));
+    
+    return (
+        <React.Fragment>
+            <IconButton edge="start" color="inherit" aria-label="Menu" onClick={handleClick}>
+                <MenuIcon />
+            </IconButton>
+            <Menu
+                id='app-menu'
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                >
+                    { Object.values(props).map(
+                        e => <MenuItem 
+                            onClick={handleClose}
+                            key={`menu-item-${e.name}`} 
+                            component={MyLink} 
+                            href={e.href}>
+                                {e.name}
+                            </MenuItem>
+)}
+             </Menu>
+        </React.Fragment>
+    );
+}
