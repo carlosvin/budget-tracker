d9877d26b7032f38478e36d607eebd5252198c77
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-09-23 06:15:43 +0200

    use frappe charts, upgrade dependencies.

--- src/components/stats/Graph.tsx
+++ src/components/stats/Graph.tsx
@@ -1,52 +1,48 @@
 import * as React from "react";
-import { VictoryPie, VictoryTheme, VictoryLine, VictoryChart } from "victory";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
+import ReactFrappeChart from "react-frappe-charts";
+import { DateDay } from "../../domain/DateDay";
 
 interface GraphProps {
     title: string;
-    data: {x: string|number|Date, y: number}[];
+    data: { x: string | number | Date, y: number }[];
 }
 
 export const GraphPie: React.FC<GraphProps> = (props) => (
     <GraphLayout title={props.title}>
-        <VictoryPie 
-            data={props.data} 
-            colorScale='qualitative'
-            theme={VictoryTheme.material}
+        <ReactFrappeChart type='percentage'
+            data={{
+                labels: props.data.map(point => point.x.toString()),
+                datasets: [{ values: props.data.map(point => point.y) }], 
+            }}
         />
     </GraphLayout>
 );
 
-export const GraphTimeLine: React.FC<GraphProps&{avg: number, expectedAvg: number}> = (props) => (
+export const GraphTimeLine: React.FC<GraphProps & { avg: number, expectedAvg: number }> = (props) => (
     <GraphLayout title={props.title}>
-        <VictoryChart scale={{x: 'time'}} >
-            <VictoryLine
-                data={props.data} 
-                theme={VictoryTheme.material}
-                interpolation='step'
-                scale={{x: 'time', y: 'log'}}
-            />
-            <VictoryLine
-                data={props.data.map(v => ({x: v.x, y: props.avg}))} 
-                style={{data: {stroke: '#4a148c', strokeWidth: 1}}}
-                scale={{x: 'time'}}
-            />
-            <VictoryLine
-                data={props.data.map(v => ({x: v.x, y: props.expectedAvg}))} 
-                style={{data: {stroke: 'red', strokeWidth: 1}}}
-                scale={{x: 'time'}}
-            />
-        </VictoryChart>
+        
+        <ReactFrappeChart
+            
+            type="line"
+            colors={["#21ba45"]}
+            axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
+            // height={250}
+            data={{
+                labels: props.data.map(point => new DateDay(point.x as Date).shortString),
+                datasets: [{ values: props.data.map(point => point.y) }], 
+            }}
+        />
     </GraphLayout>
 );
 
-export const GraphLayout: React.FC<{title: string}> = (props) => (
+export const GraphLayout: React.FC<{ title: string }> = (props) => (
     <Card>
         <CardHeader title={props.title} />
         <CardContent>
-            { props.children }
+            {props.children}
         </CardContent>
     </Card>
 );
