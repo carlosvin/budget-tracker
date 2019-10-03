2b775a7838f9d0e2527c4f279cf1f0b88251cdd3
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-10-03 06:49:27 +0200

    Use import card to keep consistency with export card

--- src/components/ImportCard.tsx
+++ src/components/ImportCard.tsx
@@ -7,12 +7,12 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import { ExportDataInfo } from './ExportDataInfo';
 
-export const ImportCard = () => {
+export const ImportCard: React.FC = () => {
 
     const [importData, setImportData] = React.useState<Partial<ExportDataSet>>();
     
     return (
-        <Card>
+        <Card style={{marginBottom: '1rem'}}>
             <CardHeader title='Import JSON'></CardHeader>
             { importData &&  <CardContent>
                 <ExportDataInfo {...importData}/>
--- src/views/ImportExport.tsx
+++ src/views/ImportExport.tsx
@@ -1,22 +1,16 @@
 import * as React from 'react';
 import { HeaderNotifierProps } from '../routes';
 import { RouterProps } from 'react-router';
-import { ImportForm } from '../components/ImportForm';
 import { ExportDataSet } from '../interfaces';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
 import { ExportCard } from '../components/ExportCard';
 import { CloseButtonHistory } from '../components/buttons/CloseButton';
-import { ExportDataInfo } from '../components/ExportDataInfo';
 import { useBudgetsStore } from '../hooks/useBudgetsStore';
 import { BudgetsStore } from '../domain/stores/interfaces';
 import { useHeaderContext } from '../hooks/useHeaderContext';
+import { ImportCard } from '../components/ImportCard';
 
 const ImportExport = (props: HeaderNotifierProps&RouterProps) => {
 
-    const [importData, setImportData] = React.useState<Partial<ExportDataSet>>();
     const [exportData, setExportData] = React.useState<ExportDataSet>();
     const {history} = props;
 
@@ -39,18 +33,9 @@ const ImportExport = (props: HeaderNotifierProps&RouterProps) => {
         
     }, [budgetsStore]);
     
-    // TODO create ImportCard as we did with ExportCard
     return (
         <React.Fragment>
-        <Card style={{marginBottom: '1rem'}}>
-            <CardHeader title='Import JSON'></CardHeader>
-            { importData &&  <CardContent>
-                <ExportDataInfo {...importData}/>
-            </CardContent> }
-            <CardActions>
-                <ImportForm onImportedData={setImportData}/>
-            </CardActions>
-        </Card>
+            <ImportCard  />
             { exportData && <ExportCard fileName='exportedData' dataToExport={exportData}/> }
         </React.Fragment>
        
