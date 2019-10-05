2abff9dabba32c5c7161b65d0d21ea3c19995811
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-10-05 17:11:30 +0200

    Revert "use https://github.com/stereobooster/react-snap to pre-render views"
    This reverts commit 22294f6f143142c4eb3000fb1d70ddedcb5d03ec.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 
 import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import { BudgetTrackerImpl } from './BudgetTrackerImpl';
-import { hydrate, render } from 'react-dom';
 
 const btApp = new BudgetTrackerImpl();
 
-const rootElement = document.getElementById('root');
-
-if (rootElement && rootElement.hasChildNodes()) {
-    hydrate(<App btApp={btApp} />, rootElement);
-} else {
-    render(<App btApp={btApp} />, rootElement);
-}
+ReactDOM.render(<App btApp={btApp}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
