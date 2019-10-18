81b2675d8374b61d0409a630816acbbedffc159e
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-10-18 08:01:58 +0200

    Updates

--- static/js/47.84493e07.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbudget-tracker"]=this["webpackJsonpbudget-tracker"]||[]).push([[47],{278:function(e,t,l){"use strict";var a=l(46);Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u=a(l(0)),d=(0,a(l(50)).default)(u.default.createElement("path",{d:"M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"}),"Flight");t.default=d}}]);
-//# sourceMappingURL=47.84493e07.chunk.js.map
--- static/js/53.c5302fa2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbudget-tracker"]=this["webpackJsonpbudget-tracker"]||[]).push([[53],{277:function(e,t,a){"use strict";var c=a(46);Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u=c(a(0)),d=(0,c(a(50)).default)(u.default.createElement("path",{d:"M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.9 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4z"}),"LocalCafe");t.default=d}}]);
-//# sourceMappingURL=53.c5302fa2.chunk.js.map
