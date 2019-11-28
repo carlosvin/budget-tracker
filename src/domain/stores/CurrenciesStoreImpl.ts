5f9769105f095e24a5dc544b93cd8a3198d33b14
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-11-28 10:55:53 +0800

    use currency names default values

--- src/domain/stores/CurrenciesStoreImpl.ts
+++ src/domain/stores/CurrenciesStoreImpl.ts
@@ -3,6 +3,7 @@ import { currenciesApi } from "../../services/CurrenciesApi";
 import { CurrenciesStore } from "./interfaces";
 import applyRate from "../utils/applyRate";
 import { Currencies } from 'currencies-map';
+import { CURRENCY_DEFAULTS } from "../../constants/currencies";
 
 export class CurrenciesStoreImpl implements CurrenciesStore {
     static readonly KEY = 'currencyRates';
@@ -17,7 +18,7 @@ export class CurrenciesStoreImpl implements CurrenciesStore {
     private _lastCurrenciesUsed?: string[];
 
     constructor() {
-        this.currencies = Currencies.names;
+        this.currencies = Currencies.names.loadDefaults(Object.entries(CURRENCY_DEFAULTS));
         this._timestamps = this.getTimestampsFromDisk();
         this._rates = this.getRatesFromDisk();
     }
