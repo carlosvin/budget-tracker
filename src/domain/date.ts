4fa178e7aa51657e96d66f96adc9fafa26185f32
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-03-14 13:20:24 +0100

    fix date diff calculations + bump dependencies

--- src/domain/date.ts
+++ src/domain/date.ts
@@ -7,7 +7,16 @@ const DAY_MS = 1000*60*60*24;
  * @param to - Ending period timestamp
  */
 export function dateDiff(from: number, to: number) {
-    return Math.floor((to - from)/DAY_MS) + 1;
+    return dateDiffObj(new Date(from), new Date(to));
+}
+
+/**
+ * Get difference between 2 dates in days, including from and to dates.
+ * @param from - Starting period Date
+ * @param to - Ending period Date
+ */
+export function dateDiffObj(from: Date, to: Date) {
+    return Math.floor((Date.UTC(to.getFullYear(), to.getMonth(), to.getDate()) - Date.UTC(from.getFullYear(), from.getMonth(), from.getDate())) /(DAY_MS)) + 1;
 }
 
 /** 
